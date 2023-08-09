7ef1ca024150b720691549e74544a34f1e30c8b5
Mariana <marianasayagosistemas@gmail.com>
2023-08-09 16:52:18 -0300

    UP dashboard con contadores y FIX paginas de error

--- Home/views.py
+++ Home/views.py
@@ -2,18 +2,26 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from Bases.models import *
 
+
 class Dashboard(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, *args, **kwargs):
-        perfiles = Perfiles.objects.filter(activo=True)
-        general = perfiles.filter(es_fiscal=False,es_voluntario=False).distinct().count()
-        voluntarios = perfiles.filter(es_voluntario=True).count()
-        fiscales = perfiles.filter(es_fiscal=True).count()
+        perfiles = Perfiles.objects.all()
+        perfiles_activos = perfiles.filter(activo=True)
+        general = perfiles_activos.filter(es_fiscal=False,es_voluntario=False).distinct().count()
+        voluntarios = perfiles_activos.filter(es_voluntario=True).count()
+        fiscales = perfiles_activos.filter(es_fiscal=True).count()
 
         context = {
+
                 'general': general,
                 'voluntarios': voluntarios,
                 'fiscales': fiscales,
+                'total': perfiles.count(),
+                'activos': perfiles_activos.count(),
+                'desactivados': perfiles.count() - perfiles_activos.count(),
                 }
+
         return context
+
